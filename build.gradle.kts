b132787880ebd61326174ced073cb6f70dcaabba
Uladzislau Ulasovich <ulasowitch.vlad@gmail.com>
2024-09-29 19:41:37 +0200

    Use Versions Catalog.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("jvm") version "2.0.0"
+    alias(libs.plugins.kotlin.jvm)
 }
 
 group = "com.github.uuladzislau"
@@ -16,6 +16,7 @@ dependencies {
 tasks.test {
     useJUnitPlatform()
 }
+
 kotlin {
     jvmToolchain(21)
 }
